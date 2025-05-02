3c598484eb032b7a38d73d402c3c161d44a3b5c9
Cedano <l21330552@hermosillo.tecnm.mx>
2025-05-02 09:21:34 -0700

    Se inicio la tarea de cinematica inversa

--- src/matlab/euler/HRz.m
+++ src/matlab/euler/HRz.m
@@ -3,9 +3,4 @@ function RHz=HRz(theta)
                    sin(theta),  cos(theta),  0,  0;
                      0,      0,      1,  0;
                      0,      0,      0,  1];          
-<<<<<<< HEAD
- end
 end
-=======
-end
->>>>>>> c8394d135c41f1ea6722438fb963d210d34f6d64
--- src/matlab/euler/rotMat2euler.m
+++ src/matlab/euler/rotMat2euler.m
@@ -9,14 +9,11 @@ secuencia = "XYZ";
 
 % Obtén las ecuaciones de la imagen
 if secuencia == "XYZ"
-<<<<<<< HEAD
     phi = atan2(-R(2,3), R(3,3));    % phi:   rotación alrededor del eje X
     theta = asin(R(1,3));     % theta: rotación alrededor del eje Y
     psi = atan2(-R(1,2), R(1,1));      % psi:   rotación alrededor del eje Z
-=======
     phi = atan(-R(2,3)/R(3,3));     % phi:   rotación alrededor del eje X
     theta = asin(R(1,3));           % theta: rotación alrededor del eje Y
     psi = atan(-R(1,2)/R(1,1));     % psi:   rotación alrededor del eje Z
->>>>>>> c8394d135c41f1ea6722438fb963d210d34f6d64
     euler = [phi;theta;psi];
 end
