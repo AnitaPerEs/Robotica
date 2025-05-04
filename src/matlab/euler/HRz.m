46011d2ed4c6b1f4c74c2b55ca39b462a7cb4abe
sebastianmtzn11 <sebastianmtzn11@gmail.com>
2025-05-03 19:00:36 -0700

    Modifiqué el archivo de cinemática_inv para realizar el código de "prueba_cinematica_inv", obtuve gráficas pero falta verificar resultados y comparar con los demás modelos de robots

--- src/matlab/euler/HRz.m
+++ src/matlab/euler/HRz.m
@@ -1,11 +1,6 @@
-function RHz=HRz(theta)
-     RHz=[ cos(theta),  -sin(theta),  0,  0;
-                   sin(theta),  cos(theta),  0,  0;
-                     0,      0,      1,  0;
-                     0,      0,      0,  1];          
-<<<<<<< HEAD
- end
+function RHz = HRz(theta)
+    RHz = [ cos(theta), -sin(theta),  0,  0;
+            sin(theta),  cos(theta),  0,  0;
+                  0,           0,     1,  0;
+                  0,           0,     0,  1 ];
 end
-=======
-end
->>>>>>> c8394d135c41f1ea6722438fb963d210d34f6d64
--- src/matlab/euler/rotMat2euler.m
+++ src/matlab/euler/rotMat2euler.m
@@ -1,22 +1,17 @@
 function euler = rotMat2euler(R, secuencia)
-%rotMat2euler Convierte una matriz de rotación a la orientación en ángulos de Euler.
+% rotMat2euler Convierte una matriz de rotación a ángulos de Euler.
+% R: matriz de rotación 3x3
+% secuencia: string como "XYZ"
 
-% Ejemplo de uso:
-R =  [sqrt(2)/4, -sqrt(2)*sqrt(3)/4, sqrt(2)/2;
-     (sqrt(2)/8) + (3/4), (sqrt(3)/4) - (sqrt(2)*sqrt(3)/8), -sqrt(2)/4;
-     (sqrt(3)/4) - (sqrt(2)*sqrt(3)/8), (3*sqrt(2)/8) + (1/4), sqrt(2)*sqrt(3)/4];
-secuencia = "XYZ";
-
-% Obtén las ecuaciones de la imagen
-if secuencia == "XYZ"
-<<<<<<< HEAD
-    phi = atan2(-R(2,3), R(3,3));    % phi:   rotación alrededor del eje X
-    theta = asin(R(1,3));     % theta: rotación alrededor del eje Y
-    psi = atan2(-R(1,2), R(1,1));      % psi:   rotación alrededor del eje Z
-=======
-    phi = atan(-R(2,3)/R(3,3));     % phi:   rotación alrededor del eje X
-    theta = asin(R(1,3));           % theta: rotación alrededor del eje Y
-    psi = atan(-R(1,2)/R(1,1));     % psi:   rotación alrededor del eje Z
->>>>>>> c8394d135c41f1ea6722438fb963d210d34f6d64
-    euler = [phi;theta;psi];
+if strcmp(secuencia, "XYZ")
+    % Convención XYZ:
+    % R = Rx(phi) * Ry(theta) * Rz(psi)
+    phi   = atan2(-R(2,3), R(3,3));  % rotación alrededor de X
+    theta = asin(R(1,3));           % rotación alrededor de Y
+    psi   = atan2(-R(1,2), R(1,1)); % rotación alrededor de Z
+    euler = [phi; theta; psi];
+else
+    error('Secuencia no soportada. Usa "XYZ".');
 end
+end
+
